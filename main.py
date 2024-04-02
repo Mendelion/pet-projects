f41db61848f05677f7411dd79768d6185b5f0fda
Nikolyamba <niko7tkc@yandex.ru>
2024-04-02 09:05:39 +0300

    Добавил кнопку для начала игры, которое переносит на новую вкладку

--- main.py
+++ main.py
@@ -9,5 +9,17 @@ root.resizable(False, False) #я думаю растягивать окно то
 label = ttk.Label(text="Добро пожаловать в нашу игру 'камень-ножницы-бумага'\nВам необходимо выбрать одно из трёх действий чтобы победить компьютер\nДля начала игры нажмите начать!", font=("Times New Roman", 14))
 label.pack()
 
+def click(): #новое окно после того как нажмём начать игру
+    root.withdraw() #скрывается первое окно
+    window = Tk()
+    window.title("Игра 'Камень-ножницы-бумага'")
+    window.geometry("800x600")
+    window.resizable(False,False)
+
+    #МАТВЕЙ!!! Теперь мы обязательно работаем только в этом окне, так как предудыщее окно закрыыто
+
+btn_start = ttk.Button(text="Начать игру", command=click)
+btn_start.place(relx=.5, rely=.5, anchor = "center", height= 80, width = 160)
+
 root.mainloop()
 
